09686d1b825040894f9776c96402a536ae195b38
Markus Fält <markus.falt@gmail.com>
2018-10-27 21:19:08 +0200

    Tog bort tråd för att skicka meddelanden.

--- client.py
+++ client.py
@@ -2,50 +2,40 @@ import socket
 import threading
 import time
 
-
-
 udp_port = 666
 multicast_ip = "192.168.0.255"
-running = True
+getThreadActive = True
+datagramReciveSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+datagramReciveSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+datagramReciveSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+datagramReciveSocket.bind(('', udp_port))
 
+def stopGetThread():
+	global getThreadActive
+	global datagramReciveSocket
+	getThreadActive = False
+	tmpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	tmpSocket.sendto("STOP".encode(), (socket.gethostname(), udp_port))
+	tmpSocket.close()
+	datagramReciveSocket.close()
 
 class getThread (threading.Thread):
-	def __init__(self, ip, port):
-		threading.Thread.__init__(self)
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		self.s.bind(('', port))
-
 	def run(self):
-		print("Run getThread")
-		while running:
-			data, addr = self.s.recvfrom(1024)
-			print("Message: " + data.decode())
-		self.s.close()
-
-class sendThread (threading.Thread):
-	def __init__(self, ip, port):
-		threading.Thread.__init__(self)
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		self.address = (ip, port)
-
-	def run(self):
-		print("Run sendThread")
-		time.sleep(5)
-		message = "Hej hej"
-		print("Sending: " + message)
-		self.s.sendto(message.encode(), self.address)
-		print(message + " sent.")
-		self.s.close()
-		running = False
+		while getThreadActive:
+			data, addr = datagramReciveSocket.recvfrom(1024)
+			if len(data.decode()) > 0 and data.decode() != 'STOP':
+				print("Message: " + data.decode())
 
 
-thread1 = getThread(multicast_ip, udp_port)
-thread2 = sendThread(multicast_ip, udp_port)
+datagramSendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+datagramSendSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-thread2.start()
+thread1 = getThread()
 thread1.start()
-thread2.join()
+
+message = "HEJ SLGKJSLGKJSLHKFDJSLDKHJLKJ"
+datagramSendSocket.sendto(message.encode(), (multicast_ip, udp_port))
+datagramSendSocket.close()
+stopGetThread()
+
 thread1.join()
